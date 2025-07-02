dca9ef48b01dd915868c87fa41a85722a767059d
Jovohir <java6923@mail.ru>
2025-07-02 06:50:20 +0500

    finish

---
+++ CRUD/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import Router from '../routers/user.route.js'
+
+const app = express();
+app.use(express.json())
+app.use('/user', Router);
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server is running ${PORT}`))
